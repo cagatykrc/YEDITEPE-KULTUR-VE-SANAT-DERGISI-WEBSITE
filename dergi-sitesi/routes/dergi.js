61d1801aa4fdfb5d87328be90d8e9ae38252dcb7
signneo <emircagataykaraca@gmail.com>
2024-01-01 14:41:49 +0300

    +

--- dergi-sitesi/routes/dergi.js
+++ dergi-sitesi/routes/dergi.js
@@ -2,32 +2,33 @@ const express = require('express');
 const router = express.Router();
 const db = require('../utility/database');
 
+// Dergi Detayı
 router.get('/:dergiId', (req, res) => {
-    const userS = req.session.user;
     const dergiId = req.params.dergiId;
-
+    
     // Dergiyi veritabanından çek
     db.query('SELECT * FROM dergiler WHERE dergi_id = ?', [dergiId], (error, results) => {
         if (error) {
             console.error('Dergi verilerini çekerken bir hata oluştu: ' + error);
             return res.status(500).send('Internal Server Error');
         }
-
+        
         if (results.length === 0) {
             // Dergi bulunamadıysa 404 hatası gönder
             return res.status(404).send('Dergi bulunamadı');
         }
-
+        
         const dergi = results[0];
+        console.log(dergi, dergiId);
         const ekleyenkisi = dergi.olusturan_user_id === 1 ? 'Admin' : 'Anonim';
+        const userS = req.session.user;
         res.render('dergiDetay', { dergi, ekleyenkisi, userS });
     });
 });
 
-module.exports = router;
 // Dergi Oluşturma Formu Gönderildiğinde
 router.post('/olustur', (req, res) => {
-    if (req.user && (req.user.role === 'admin' || req.user.role === 'moderator')) {
+    if (req.user && (req.user.role === 'admin')) {
         const { konu, aciklama, resim, indirmeLinki } = req.body;
         const olusturanUserId = req.user.user_id;
 
@@ -52,4 +53,5 @@ router.post('/olustur', (req, res) => {
         res.redirect('/');
     }
 });
+
 module.exports = router;
