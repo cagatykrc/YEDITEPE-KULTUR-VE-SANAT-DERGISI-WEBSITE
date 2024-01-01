8c1658bb09ede5d1ce8fa64dc354fa414db3f531
signneo <emircagataykaraca@gmail.com>
2024-01-01 18:09:00 +0300

    dergiDetay Sistemi yazildi

--- dergi-sitesi/routes/dergi.js
+++ dergi-sitesi/routes/dergi.js
@@ -3,27 +3,14 @@ const router = express.Router();
 const db = require('../utility/database');
 
 // Dergi Detayı
-router.get('/:dergiId', (req, res) => {
+router.get('/:dergiId', async (req, res) => {
     const dergiId = req.params.dergiId;
-    
-    // Dergiyi veritabanından çek
-    db.query('SELECT * FROM dergiler WHERE dergi_id = ?', [dergiId], (error, results) => {
-        if (error) {
-            console.error('Dergi verilerini çekerken bir hata oluştu: ' + error);
-            return res.status(500).send('Internal Server Error');
-        }
-        
-        if (results.length === 0) {
-            // Dergi bulunamadıysa 404 hatası gönder
-            return res.status(404).send('Dergi bulunamadı');
-        }
-        
-        const dergi = results[0];
-        console.log(dergi, dergiId);
-        const ekleyenkisi = dergi.olusturan_user_id === 1 ? 'Admin' : 'Anonim';
-        const userS = req.session.user;
-        res.render('dergiDetay', { dergi, ekleyenkisi, userS });
-    });
+    console.log('İşlem başladı');
+    const [results] = await db.query('SELECT * FROM dergiler WHERE dergi_id = ?', [dergiId]);
+    const dergi = results[0];
+    console.log(dergi, dergiId);
+    const userS = req.session.user;
+    res.render('dergiDetay', { dergi, userS });
 });
 
 // Dergi Oluşturma Formu Gönderildiğinde
