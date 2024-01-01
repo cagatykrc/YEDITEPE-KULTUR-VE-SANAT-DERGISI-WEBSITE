35450b4f9c7882d6b4f3183473cd0cfaf2476819
signneo <emircagataykaraca@gmail.com>
2024-01-01 13:25:52 +0300

    +

--- dergi-sitesi/routes/dergi.js
+++ dergi-sitesi/routes/dergi.js
@@ -3,20 +3,24 @@ const router = express.Router();
 const db = require('../utility/database');
 
 router.get('/:dergiId', (req, res) => {
+    const userS = req.session.user;
     const dergiId = req.params.dergiId;
+
     // Dergiyi veritabanından çek
     db.query('SELECT * FROM dergiler WHERE dergi_id = ?', [dergiId], (error, results) => {
         if (error) {
             console.error('Dergi verilerini çekerken bir hata oluştu: ' + error);
             return res.status(500).send('Internal Server Error');
         }
+
         if (results.length === 0) {
             // Dergi bulunamadıysa 404 hatası gönder
             return res.status(404).send('Dergi bulunamadı');
         }
+
         const dergi = results[0];
         const ekleyenkisi = dergi.olusturan_user_id === 1 ? 'Admin' : 'Anonim';
-        res.render('dergiDetay', { dergi, ekleyenkisi });
+        res.render('dergiDetay', { dergi, ekleyenkisi, userS });
     });
 });
 
