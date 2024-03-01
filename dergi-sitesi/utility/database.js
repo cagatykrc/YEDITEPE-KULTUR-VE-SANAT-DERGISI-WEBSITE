18acaedda2df795ec24b44ae7a92094e7842e4bd
Sinan Kogce <sinankogce@Sinans-MacBook-Pro.local>
2024-03-02 00:11:49 +0300

    gitignore activation

--- dergi-sitesi/utility/database.js
+++ dergi-sitesi/utility/database.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+/* const dotenv = require('dotenv');
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
@@ -14,5 +14,21 @@ require('dotenv').config();
 const sequelize = new Sequelize( 'ykvs_books', 'root', 'XLIE4244hGW-TV', {
   host:'localhost',
   dialect:'mysql'
+});
+module.exports = sequelize;   */
+
+
+const dotenv = require('dotenv');
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER , process.env.DB_PASS, {
+//   host: process.env.DB_HOST ,
+//   dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+// });
+const sequelize = new Sequelize( 'ykvs_books', 'epson', 'epson', {
+  host:'localhost',
+  dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 module.exports = sequelize;  
