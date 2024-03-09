71e87e582600ff230e216d35a0dc6a650e4a1834
Sinan Kogce <sinankogce@Sinans-MacBook-Pro.local>
2024-03-09 23:25:45 +0300

    deployment preparation

--- dergi-sitesi/utility/database.js
+++ dergi-sitesi/utility/database.js
@@ -1,23 +1,3 @@
-/* const dotenv = require('dotenv');
-require('dotenv').config();
-const mysql = require('mysql2/promise');
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER , process.env.DB_PASS, {
-//   host: process.env.DB_HOST ,
-//   dialect:'mysql' 
-// });
-
-
-const sequelize = new Sequelize( 'ykvs_books', 'root', 'XLIE4244hGW-TV', {
-  host:'localhost',
-  dialect:'mysql'
-});
-module.exports = sequelize;   */
-
-
 const dotenv = require('dotenv');
 require('dotenv').config();
 const mysql = require('mysql2/promise');
@@ -27,8 +7,8 @@ require('dotenv').config();
 //   host: process.env.DB_HOST ,
 //   dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 // });
-const sequelize = new Sequelize( 'ykvs_books', 'root', 'YKVSD123456', {
-  host:'37.148.211.179',
+const sequelize = new Sequelize( 'ykvs_books', 'root', 'XLIE4244hGW-TV', {
+  host:'localhost',
   dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
-module.exports = sequelize;
+module.exports = sequelize;  
